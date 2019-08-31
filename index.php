798a9cfc783da82e0687c0a01a39ce2e23b35bd7
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-08-30 22:44:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
-<?php include 'functions.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Random Quotes</title>
-  <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-  <div class="container">
-    <div id="quote-box">
-      <?php
-      function printQuote() {
-      ?>
+  <?php include 'functions.php'; ?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Random Quotes</title>
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <div class="container">
+      <div id="quote-box">
+        <?php
+        printQuote();
+        ?>
+      </div>
+      <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
     </div>
-    <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
-  </div>
-</body>
-</html>
+  </body>
+  </html>
