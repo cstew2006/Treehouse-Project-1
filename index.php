a31188e450928d408430c5a949261e67756c752a
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-08-30 22:09:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'include 'inc/functions.php'; ?>
+<?php include 'functions.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,10 +12,8 @@
   <div class="container">
     <div id="quote-box">
       <?php
-      include 'inc/functions.php';
+      function printQuote() {
       ?>
-      <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-      <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
     </div>
     <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
   </div>
